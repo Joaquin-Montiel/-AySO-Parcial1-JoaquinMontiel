aaeff9a11f94a9aeaa28d52ff31272db7c3a26e4
Joaquin-Montiel <joacomontiel94@gmail.com>
2024-10-07 03:30:20 +0000

    Punto A

---
+++ parcial1/puntoA.sh
@@ -0,0 +1,24 @@
+vagrant@VMParcial:~/parcialpruebas$ mkdir -p Examenes-UTN/{profesores,alumno_{1..3}/parcial_{1..3}}
+vagrant@VMParcial:~/parcialpruebas$ ll
+total 12
+drwxrwxr-x 3 vagrant vagrant 4096 Oct  6 21:02 ./
+drwxr-x--- 8 vagrant vagrant 4096 Oct  6 21:01 ../
+drwxrwxr-x 6 vagrant vagrant 4096 Oct  6 21:02 Examenes-UTN/
+vagrant@VMParcial:~/parcialpruebas$ tree Examenes-UTN
+Examenes-UTN
+├── alumno_1
+│   ├── parcial_1
+│   ├── parcial_2
+│   └── parcial_3
+├── alumno_2
+│   ├── parcial_1
+│   ├── parcial_2
+│   └── parcial_3
+├── alumno_3
+│   ├── parcial_1
+│   ├── parcial_2
+│   └── parcial_3
+└── profesores
+
+14 directories, 0 files
+
